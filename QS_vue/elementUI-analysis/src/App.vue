5f713ce27e1918279f8f5934076755870f5ad65d
zhuRon <940223872@qq.com>
2018-01-13 19:37:05 +0800

    element 组件实现

---
+++ QS_vue/elementUI-analysis/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  
+  <div id="app">
+    <el-badge class="item" :value="10" :max="9">
+    <el-button :disabled="saveDisabled" nativeType="submit" size="medium" type="primary" @click="addComment" >点我</el-button>
+    </el-badge>
+  </div>
+</template>
+
+<script>
+import ElBadge from './components/ElBadge'
+import ElButton from './components/ElButton.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      saveDisabled:true
+    }
+  },
+  mounted(){
+    setTimeout(() => {
+      this.saveDisabled = false;
+    }, 5000);
+  },
+  methods:{
+    addComment(){
+      console.log('添加评论')
+    }
+  },
+  components: {
+    ElButton,
+    ElBadge
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ QS_vue/elementUI-analysis/src/components/ElBadge.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="el-badge"> 
+    <slot></slot>
+    <transition name="el-zoom-in-center">    </transition>
+        <sup v-show="!hidden&&(content||isDot)" class="el-badge__content" :class="{'is-fixed':true, 'is-dot':isDot}" v-text="content"></sup>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'ElBadge',
+    props:{
+        value:{},
+        max:Number,
+        isDot:Boolean,
+        hidden:Boolean
+    },
+    computed:{
+        content(){
+            if(this.isDot)return;
+            const value =this.value
+            const max = this.max
+            if(typeof value === 'number' && typeof max ==='number'){
+                return max< value ?`${max}+`:value;
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
